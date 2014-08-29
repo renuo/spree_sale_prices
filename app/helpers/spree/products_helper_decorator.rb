075b3824935c9d916eaf49ba669af90d3e07c6dd
Jason Stokes <jpstokes@plecco.net>
2014-08-29 18:05:16 -0500

    changed sale percentage to use msrp instead of original_price to figure out sales percentage

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -1,3 +1,5 @@
 def discount_percentage(product)
-  "#{((1 - product.price.to_f / product.original_price.to_f) * 100).round}%"
+  if product.master.msrp.present?
+    "#{((1 - product.price.to_f / product.master.msrp.to_f) * 100).round}%"
+  end
 end
