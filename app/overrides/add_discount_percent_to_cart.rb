2016b254ad708d468c5d7b5f8d13f9e997068250
Jason Stokes <jpstokes@plecco.net>
2014-08-29 17:13:32 -0500

    add msrp to variant index and edit pages

--- app/overrides/add_discount_percent_to_cart.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(virtual_path: "spree/orders/_line_item",
-                     name: "add_discount_percent_to_cart",
-                     replace_contents: ".cart-item-discount",
-                     partial: "spree/views/orders/cart_item_discount",
-                     disabled: false)
-
