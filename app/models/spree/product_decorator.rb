f8178e0001f41ce4bdf9c0380f1e5fe2a1134d79
Josua Schmid <josua.schmid@renuo.ch>
2015-03-09 16:11:55 +0100

    updated comment

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,7 +3,8 @@ Spree::Product.class_eval do
   has_many :sale_prices, through: :prices
 
   # Essentially all read values here are delegated to reading the value on the Master variant
-  # All write values will write to all variants (including the Master) unless that method's all_variants parameter is set to false, in which case it will only write to the Master variant
+  # All write values will write to all variants (including the Master) unless that method's all_variants parameter is
+  # set to false, in which case it will only write to the Master variant.
 
   delegate_belongs_to :master, :active_sale_in, :current_sale_in, :next_active_sale_in, :next_current_sale_in,
                       :sale_price_in, :on_sale_in?, :original_price_in, :discount_percent_in, :discount_percent, :sale_price,
