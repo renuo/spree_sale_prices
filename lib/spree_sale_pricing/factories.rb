81729e52243f12fae5cd2a685e197e3e88a92264
Josua Schmid <josua.schmid@renuo.ch>
2015-03-10 11:09:41 +0100

    added factory for sale_price

--- lib/spree_sale_pricing/factories.rb
+++ lib/spree_sale_pricing/factories.rb
@@ -3,4 +3,13 @@ FactoryGirl.define do
   #
   # Example adding this to your spec_helper will load these Factories for use:
   # require 'spree_sale_pricing/factories'
+
+  factory :sale_price, class: Spree::SalePrice do
+    value { BigDecimal.new(15.99, 4) }
+    start_at { Time.now }
+    end_at nil
+    enabled true
+    calculator { Spree::Calculator::FixedAmountSalePriceCalculator.new }
+  end
+
 end
