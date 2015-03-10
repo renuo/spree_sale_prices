319c84caed40a460beb2826f588154580641aefa
Josua Schmid <josua.schmid@renuo.ch>
2015-03-10 10:46:02 +0100

    moved product specs

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Spree::SalePrice do
+
+  it 'can put a product on fixed sale' do
+    product = create(:base_product)
+    expect(product.price).to eql 19.99
+    expect(product.original_price).to eql 19.99
+    expect(product.on_sale?).to be false
+
+    product.put_on_sale 10.95
+
+    expect(product.price).to eql 10.95
+    expect(product.original_price).to eql 19.99
+    expect(product.on_sale?).to be true
+  end
+
+  it 'can put a product on percentage sale' do
+    product = create(:base_product)
+    expect(product.price).to eql 19.99
+    expect(product.original_price).to eql 19.99
+    expect(product.on_sale?).to be false
+
+    product.put_on_sale 0.2, { calculator_type: Spree::Calculator::PercentOffSalePriceCalculator.new }
+
+    expect(product.price).to be_within(0.01).of(15.99)
+    expect(product.original_price).to eql(19.99)
+    expect(product.on_sale?).to be true
+  end
+
+end
