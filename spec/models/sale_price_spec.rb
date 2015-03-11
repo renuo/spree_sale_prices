f43b34f5bcaf70cc5a3154dfd6f97e8c11b407cc
Josua Schmid <josua.schmid@renuo.ch>
2015-03-11 10:43:25 +0100

    introduced sale price specs

--- spec/models/sale_price_spec.rb
+++ spec/models/sale_price_spec.rb
@@ -2,6 +2,37 @@ require 'spec_helper'
 
 describe Spree::SalePrice do
 
+  it 'can start and end never' do
+    sale_price = build(:sale_price)
+    sale_price.start
 
+    expect(sale_price).to be_enabled
+    expect(sale_price.end_at).to be(nil)
+  end
+
+  it 'can start and then end at a specific time' do
+    sale_price = build(:sale_price)
+    sale_price.start(1.day.from_now)
+
+    expect(sale_price).to be_enabled
+    expect(sale_price.end_at).to be_within(1.second).of(1.day.from_now)
+  end
+
+  it 'can stop' do
+    sale_price = build(:active_sale_price)
+    sale_price.stop
+
+    expect(sale_price).not_to be_enabled
+    expect(sale_price.end_at).to be_within(1.second).of(Time.now)
+  end
+
+  it 'can create a money price ready to display' do
+    sale_price = build(:active_sale_price)
+    money = sale_price.display_price
+
+    expect(money).to be_a Spree::Money
+    expect(money.money.amount).to eq(sale_price.price)
+    #expect(money.money.currency).to eq(sale_price.currency)
+  end
 
 end
